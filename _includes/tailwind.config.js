0f609fa93dba79d70622a9c2858f9fb375543131
Jerric Lyns John <jerriclynsjohn@outlook.com>
2020-03-07 02:36:59 +0530

    non regular config

--- _includes/tailwind.config.js
+++ _includes/tailwind.config.js
@@ -1,6 +1,17 @@
 module.exports = {
     theme: {
+        fontFamily: {
+            sans: ['Montserrat', 'sans-serif'],
+        },
         extend: {
+            borderRadius: {
+                large: '1rem',
+            },
+            spacing: {
+                '72': '18rem',
+                '84': '21rem',
+                '96': '24rem',
+            },
         },
     },
     variants: {},
